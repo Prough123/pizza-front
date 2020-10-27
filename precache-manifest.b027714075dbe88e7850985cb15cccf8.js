af7220d3ddd0d2af2f989892a36f8c61c4e8338f
Prough123 <slipok1999@mail.ru>
2020-10-27 20:03:13 +0300

    Updates

---
+++ precache-manifest.b027714075dbe88e7850985cb15cccf8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405eb55c000278ca08ea8cfbbc2f64d5",
+    "url": "/pizza-front/index.html"
+  },
+  {
+    "revision": "4a7fac534c80afe508cb",
+    "url": "/pizza-front/static/css/2.8fd988a0.chunk.css"
+  },
+  {
+    "revision": "69d2e80d7d0eb8b9e445",
+    "url": "/pizza-front/static/css/main.1893bece.chunk.css"
+  },
+  {
+    "revision": "4a7fac534c80afe508cb",
+    "url": "/pizza-front/static/js/2.8f067e66.chunk.js"
+  },
+  {
+    "revision": "69d2e80d7d0eb8b9e445",
+    "url": "/pizza-front/static/js/main.fe03c64b.chunk.js"
+  },
+  {
+    "revision": "0a634ab2a911b156730c",
+    "url": "/pizza-front/static/js/runtime~main.70f44ace.js"
+  },
+  {
+    "revision": "7eb83a9a67259c1b0a753df26ce837f5",
+    "url": "/pizza-front/static/media/1.7eb83a9a.png"
+  },
+  {
+    "revision": "6884eef99509d65ed057d539ee029b7d",
+    "url": "/pizza-front/static/media/16397.6884eef9.otf"
+  },
+  {
+    "revision": "8d501cd06bc3e1e2a253efaf0e39544a",
+    "url": "/pizza-front/static/media/2.8d501cd0.png"
+  }
+]);
